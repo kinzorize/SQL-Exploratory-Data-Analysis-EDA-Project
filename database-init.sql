0e70ac9f83b4e0da21a0708bae7b37e53f602135
kingsley Uchechukwu Elijah <77132974+kinzorize@users.noreply.github.com>
2025-04-11 01:04:05 -0600

    Update database-init.sql

--- database-init.sql
+++ database-init.sql
@@ -253,3 +253,49 @@ GROUP BY
 c.country
 ORDER BY total_sold_items DESC
 ===========================================================================
+--Which 5 products generate the highest revenue?
+SELECT TOP 5
+p.product_name,
+SUM(f.sales_amount) total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC
+
+SELECT *
+FROM (
+SELECT
+p.product_name,
+SUM(f.sales_amount) total_revenue,
+ROW_NUMBER() OVER (ORDER BY SUM(f.sales_amount) DESC) AS rank_products
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name)t
+WHERE rank_products <= 5
+
+-- What are the 5 worst-performing products in terms of sales?
+SELECT TOP 5
+p.product_name,
+SUM(f.sales_amount) total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue 
+
+--The 3 customers with the fewest orders placed
+SELECT TOP 3
+c.customer_key,
+c.first_name,
+c.last_name,
+COUNT (DISTINCT order_number) AS total_orders
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+GROUP BY 
+c.customer_key,
+c.first_name,
+c.last_name
+ORDER BY total_orders
